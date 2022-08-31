ff2a7d9e45725209303820c23afb8dcde8e680f3
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-08-31 18:47:10 -0300

    Delete Trainning directory

--- Trainning/#061 - PA v2.0.py
+++
@@ -1,8 +0,0 @@
-p = int(input('Primeiro termo da PA: '))
-r = int(input('Razao da PA: '))
-d = p + (r*10)
-while p != d:
-    print(f'{p}', end=' -> ')
-    p += r
-print('Fim')
-
--- Trainning/FileHandling.py
+++
@@ -1,7 +0,0 @@
-import os
-
-filepath = os.path.abspath('Documents/emailvagas.txt')
-
-file = open(filepath, 'r')
-print(file.read())
-
