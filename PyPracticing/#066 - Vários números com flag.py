4344c9a82320d83a348d2377a2e1b8aacb86a326
felixoakz <feliz@oakz.org>
2022-09-01 07:32:14 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/#066 - Vários números com flag.py
+++ PyPracticing/#066 - Vários números com flag.py
@@ -1,5 +1,13 @@
 '''
 ler varios numeros inteiros pelo teclado
-so parar quando user digitar cond parada: 999
+so parar quando user digitar condic. parada: 999
 final mostrar quantos n digitados e soma entre eles (sem o flag)
 '''
+cont = som = 0
+while True:
+    num = int(input('Digite um numero inteiro: '))
+    cont += 1
+    som += num
+    if num == 999:
+        break
+print(f'Foram digitados {cont} numeros e a soma entre eles resulta em {som-999}')
