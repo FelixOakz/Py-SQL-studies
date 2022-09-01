70105ed4b26b90c1f88d2cdcb8d2c1e5c393d83e
felixoakz <feliz@oakz.org>
2022-09-01 08:44:47 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ PyPracticing/#067 - Tabuada v3.0.py
@@ -0,0 +1,5 @@
+'''
+mostrar tabuada de varios n, um de cada vez, pra cada valor digitado pelo user
+programa sera interrompido, qnd o n digitado for negativo
+'Quer ver a tabuada de que valor?' por ex, de 3: 3 x 1 ate 3 x 10..e continua perguntando
+'''
