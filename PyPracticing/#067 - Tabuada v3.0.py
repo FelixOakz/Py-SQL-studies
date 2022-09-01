9a1ad3ead3a3965237b62cf069e3a009e981fa42
felixoakz <feliz@oakz.org>
2022-09-01 09:19:40 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/#067 - Tabuada v3.0.py
+++ PyPracticing/#067 - Tabuada v3.0.py
@@ -1,5 +1,7 @@
-'''
-mostrar tabuada de varios n, um de cada vez, pra cada valor digitado pelo user
-programa sera interrompido, qnd o n digitado for negativo
-'Quer ver a tabuada de que valor?' por ex, de 3: 3 x 1 ate 3 x 10..e continua perguntando
-'''
+while True:
+    val = int(input('Quer ver a tabuada de que valor?: '))
+    if val < 0:
+        break
+    for i in range(1, 11):
+        print(f'{val} * {i} = {val*i}')
+print('Programa de tabuada encerrado.')
