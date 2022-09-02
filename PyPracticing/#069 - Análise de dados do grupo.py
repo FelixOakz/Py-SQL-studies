4bc6a6eb8559e209fb3f1dd00fe7460db38eadc2
felixoakz <feliz@oakz.org>
2022-09-02 07:11:03 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ PyPracticing/#069 - Análise de dados do grupo.py
@@ -0,0 +1,11 @@
+"""
+ler idade e sexo de varias pessoas
+
+a cada pessoa, o programa deve perguntar se quer ou nao continuar
+
+e ao final mostre:
+a) quantas pessoas com mais de 18 anos
+b) quantos homens cadastrados
+c) quantas Mulheres tem menos de 20 anos.
+
+"""
--- PyPracticing/Draft.py
+++ PyPracticing/Draft.py
@@ -1,2 +1,3 @@
-for i in range(1,5):
-    print(e:=1)
+oi = 'felix'
+
+print(oi.strip()[0])
