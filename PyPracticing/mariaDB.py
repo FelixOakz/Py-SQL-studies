f7444569eba3a0f5353fdb6a4ba4196c59f94da1
felixoakz <feliz@oakz.org>
2022-09-04 11:17:36 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/mariaDB.py
+++ PyPracticing/mariaDB.py
@@ -1,21 +1,23 @@
-# Module Imports
-import mariadb
-import sys
+import mysql.connector
 
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user="db_user",
-        password="db_user_passwd",
-        host="192.0.2.1",
-        port=3306,
-        database="employees"
+myDB = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'timemachine',
+    database = 'pythonx')
 
-    )
-    print(conn)
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+myCur = myDB.cursor()
 
-# Get Cursor
-cur = conn.cursor()
+cur.execute('INSERT INTO user VALUES(1516,'
+            'victoria',
+            'vic@gmail.com',
+            'PRO')')
+
+myDB.commit()
+
+cur.execute('SELECT * FROM user')
+result = cur.fetchall()
+for i in result:
+    print(i)
+
+myDB.close()
