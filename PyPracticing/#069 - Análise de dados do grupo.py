4fea70d15917fc77125807876fbdcee59716bbea
felixoakz <feliz@oakz.org>
2022-09-05 09:02:04 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/#069 - Análise de dados do grupo.py
+++ PyPracticing/#069 - Análise de dados do grupo.py
@@ -1,32 +1,17 @@
-"""
-ler idade e sexo de varias pessoas
-
-a cada pessoa, o programa deve perguntar se quer ou nao continuar
-
-
-
-
-e ao final mostre:
-a) quantas pessoas com mais de 18 anos
-b) quantos homens cadastrados
-c) quantas Mulheres tem menos de 20 anos.
-
-"""
-maiores = homi = mulhermen = 0
+maiores = homens = mulhermenor = 0
 while True:
     nom = str(input('Nome?: ')).strip()
     id = int(input('Idade?: '))
     sex = str(input('Sexo?: ')).strip().upper()[0]
+    if id >= 18:
+        maiores += 1
+    if sex == 'M':
+        homens += 1
+    if sex == 'F' and id <= 20:
+        mulhermenor += 1
     opt = str(input('Deseja continuar?[S/N]: ')).strip().upper()[0]
-    if 'S' in opt:
-        if id >= 18:
-            maiores += 1
-        elif sex == 'Mm':
-            homi += 1
-        elif sex == 'F' and id <= 20:
-            mulhermen += 1
-    else:
+    if opt == 'N':
         break
-print(f'\nDas pessoas entrevistadas, temos {maiores} maiores de idade,'
-      f'{homi} Homens cadastrados,'
-      f'E {mulhermen} mulheres com menos de 20 anos.')
+print(f'\nDas pessoas entrevistadas: Temos {maiores} maiores de idade,'
+      f' {homens} Homens cadastrados,'
+      f' e {mulhermenor} mulheres com menos de 20 anos.')
