24e08dd5705eb6802a44aaee7ae47bca2c5b0dc9
felixoakz <feliz@oakz.org>
2022-09-05 12:12:44 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/#070 - Estatísticas em produtos.py
+++ PyPracticing/#070 - Estatísticas em produtos.py
@@ -1,25 +1,21 @@
-tot = cont = prodB = prodC = 0
-prodBNome = ' '
+tot = valormil = menor = cont = 0
+barato = ''
 while True:
-    produto = str(input('Digite o produto: ')).strip().upper()
-    preco = int(input('Digite o valor do produto: '))
-
+    produto = str(input('Digite o produto: '))
+    preco = int(input('Digite o valor do produto [R$]: '))
+    cont += 1
     tot += preco
-
-    if preco > 1000:
-        prodC += 1
-
-    if preco < prodB:
-        prodB = preco
-        prodBNome = produto
-
-    resp = ' '
+        if preco > 1000:
+        valormil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+     resp = ' '
     while resp not in 'SN':
         resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
     if resp == 'N':
         break
-
 print(f'\n- - - Resumo da Compra - - -')
 print(f'O total da compra foi de R${tot:.2f}')
-print(f'Temos {prodC} produtos custando mais de R$1000')
-print(f'O produto mais barato foi {prodBNome} que custa R${prodB}')
+print(f'Temos {valormil} produtos custando mais de R$1000')
+print(f'O produto mais barato foi {barato} que custa R${menor}')
