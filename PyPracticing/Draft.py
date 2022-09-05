0842abffa119f68464567c11a5343e2aabdf0792
felixoakz <feliz@oakz.org>
2022-09-05 20:25:53 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Draft.py
+++ PyPracticing/Draft.py
@@ -1,17 +1,12 @@
-n = int(input("enter n: "))
+import mysql.connector
 
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='timemachine'
+)
+cursor = conn.cursor()
 
-def Fibonacci(n):
-    if n<0:
-        print("Incorrect input")
-
-    elif n==1:
-     return 0
-
-    elif n==2:
-     return 1
-
-    else:
-      return Fibonacci(n-1)+Fibonacci(n-2)
-
-print(n)
+cursor.execute(
+    'drop table '
+)
