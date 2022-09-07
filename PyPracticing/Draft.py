8597ec1c55b257a9f50e9d7dbb582300ab23054d
felixoakz <feliz@oakz.org>
2022-09-07 18:18:58 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Draft.py
+++ PyPracticing/Draft.py
@@ -1,19 +1,1 @@
-from tkinter import *
-from PIL import ImageTk, Image
 
-window = Tk()
-
-name = Label(window, text='Rapaz Desaparecido!!!', bg='white', fg='blue', font=('Serif', 20))
-img = Image.open('ram.png')
-logo = ImageTk.PhotoImage(img)
-image = Label(window, image=logo)
-button = Button(window, text='AJUDAR')
-username = Label(window, text='Username', font=('Serif', 12))
-inputBox = Entry(window)
-
-name.pack()
-image.pack()
-button.place(x=130, y=350)
-username.pack(side=LEFT)
-inputBox.pack(side=RIGHT)
-window.mainloop()
