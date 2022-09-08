5920876dbfa85d4c51163eac69cfdee903b012e6
felixoakz <feliz@oakz.org>
2022-09-08 09:49:10 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/#071 - Simulador de CX eletronico.py
+++ PyPracticing/#071 - Simulador de CX eletronico.py
@@ -15,5 +15,16 @@ print('-'*30)
 print(f"{'BDB - Banco dos Brothers':^30}")
 print('-'*30)
 
+notas50 = notas20 = notas10 = notas1 = 0
 while True:
+
     valor = int(input('Insira o valor que deseja sacar(R$): '))
+    while valor % 50:
+        notas50 += 1
+    break
+
+print(f'\n> Serao entregues {notas50} notas de R$50,\n'
+      f'{notas20} notas de R$20,\n'
+      f'{notas10} notas de R$10,\n'
+      f'{notas1} notas de R$1. Bom dia!'
+      )
