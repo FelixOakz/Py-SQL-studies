c1ea42d975c022088ccdeea90cad8b76dcee4b6a
felixoakz <feliz@oakz.org>
2022-09-12 11:48:52 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ PyPracticing/#073 - Tuplas com Times de Futebol.py
@@ -0,0 +1,30 @@
+"""
+tupla com os 20 primeiros colocados camp. brasileiro
+a: apenas os 5 primeiros colocados
+b: os ultimos 4 colocados
+c: uma lista com os times em ordem alfabetica
+d: em que posical da tabela esta o time chapecoense
+"""
+
+times =(
+	'PALMEIRAS',
+	'INTERNACIONAL',
+	'FLAMENGO',
+	'FLUMINENSE',
+	'CORINTHIANS',
+	'ATHLETICO-PR',
+	'ATLETICO-MG',
+	'AMERICA-MG',
+	'GOIAS',
+	'SANTOS',
+	'RED BULL BRAGANTINO',
+	'BOTAFOGO',
+	'SAO PAULO',
+	'CEARA',
+	'FORTALEZA',
+	'CORITIBA',
+	'CUIABA',
+	'AVAI',
+	'ATHLETICO-GO',
+	'JUVENTUDE'
+)
