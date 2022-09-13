c3ec586070bb4793ca43fadd30f17d69d96ae44a
felixoakz <feliz@oakz.org>
2022-09-13 11:38:20 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/#075 - Análise de dados em uma Tupla.py
+++ PyPracticing/#075 - Análise de dados em uma Tupla.py
@@ -1,7 +1,13 @@
-"""
-programa que leia 4 valores pelo teclado e guarde-os em uma tupla,
-1 - quantas vezes apareceu o valor 9
-em qua posical foi digitado o primeiro valor 3
-quantos foram os numeros pares
-
-"""
+tuple = ()
+par = 0
+for val in range(1, 5):
+	num = int(input(f'qual o {val} valor? '))
+	tuple = tuple + (num,)
+	if num % 2 == 0:
+		par += 1
+print(f'O valor 9 apareceu {tuple.count(9)} vezes.')
+if 3 in tuple:
+	print(f'O valor 3 foi digitado na posicao {tuple.index(3)+1}')
+else:
+	print('O valor 3 nao foi digitado.')
+print(f'Apareceram {par} numeros pares.')
