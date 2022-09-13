5ded8152d6419b354d47f03747df6df7442d5158
felixoakz <feliz@oakz.org>
2022-09-13 09:36:12 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ PyPracticing/#075 - Análise de dados em uma Tupla.py
@@ -0,0 +1,7 @@
+"""
+programa que leia 4 valores pelo teclado e guarde-os em uma tupla,
+1 - quantas vezes apareceu o valor 9
+em qua posical foi digitado o primeiro valor 3
+quantos foram os numeros pares
+
+"""
