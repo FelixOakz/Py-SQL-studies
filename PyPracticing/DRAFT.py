80a4a4693ccc7fc11eb88e1cadddbc675c1f43dd
felixoakz <feliz@oakz.org>
2022-09-19 11:13:33 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/DRAFT.py
+++
@@ -1,8 +0,0 @@
-lanche = ['burguer', 'refri', 'pizza', 'pudim']
-print(lanche)
-lanche[3] = 'ice cream'
-print(lanche)
-print(lanche[2])
-lanche.insert(1, 'hot dog')
-print(lanche)
-print(lanche[2])
