8cebb333ea2c94295a609aecaa21f566dd41bbc4
felixoakz <feliz@oakz.org>
2022-09-20 06:59:05 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ PyPracticing/Guanabara/#082 - Dividindo valores em várias listas.py
@@ -0,0 +1,11 @@
+lista = []
+while True:
+	lista.append(int(input('Digite um numero para adicionar a lista: ')))
+	r = str(input('deseja continuar? [S/N]')).strip().upper()
+	if r == 'N':
+		break
+par = [x for x in lista if x % 2 == 0]
+impar = [x for x in lista if x % 2 != 0]
+print(f'A lista completa: {lista}')
+print(f'a lista de pares: {par}')
+print(f'a lista de impares: {impar}')
