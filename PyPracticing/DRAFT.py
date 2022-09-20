edd23160c45785b06fc5e4471d76eeb1fa2f539f
felixoakz <feliz@oakz.org>
2022-09-20 10:07:36 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/DRAFT.py
+++ PyPracticing/DRAFT.py
@@ -1,4 +1,16 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(lista)
-lista.reverse()
-print(lista)
+#DICTIONARIES
+# A simple dictionary
+person = {"age": "21", "gender": "female"}
+# Accessing a value
+print("The person's age is " + person["age"])
+# Adding a new key-value pair
+person["height"] = 170
+# Looping through all key-value pairs ages = { "Stacy": 20, "Michelle": 21}
+for name, age in ages.items():
+	print(name + " is " + str(age))
+# Looping through all keys ages = { "Stacy": 20, "Michelle": 21}
+for name in ages.keys():
+	print(name + " is a name")
+# Looping through all the values ages = { "Stacy": 20, "Michelle": 21}
+for age in ages.values():
+	print(str(num) + " is my age")
