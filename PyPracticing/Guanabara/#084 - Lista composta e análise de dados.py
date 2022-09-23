3668f4af5d889b7c89371e06753bcd348387c755
felixoakz <feliz@oakz.org>
2022-09-23 09:46:22 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#084 - Lista composta e análise de dados.py
+++ PyPracticing/Guanabara/#084 - Lista composta e análise de dados.py
@@ -1,18 +1,17 @@
 entrada = []
 pessoas = []
-cont = 0
+cont = menor = maior = 0
 while True:
 	entrada.append(str(input('Qual seu nome?: ')))
 	entrada.append(str(input('Qual seu peso?: ')))
-
 	pessoas.append(entrada[:])
 	cont += 1
 	entrada.clear()
-	r = entrada('Deseja continuar? [S/N]: ').strip()
+	r = str(input('Deseja continuar? [S/N]: ')).strip()
 	if r in 'Nn':
 		break
 print('-'*40)
-print(f'Os dados foram {pessoas}'
-print(f'Ao todo, foram cadastradas {len(pessoas) pessoas.}')
-print(f'Maior peso foi de {}kg, peso de {}.')
-print(f'Menor peso foi de {}kg, peso de {}.')
+print(f'Os dados foram {pessoas}')
+print(f'Ao todo, foram cadastradas {len(pessoas)} pessoas.')
+#print(f'Maior peso foi de {}kg, peso de {}.')
+#print(f'Menor peso foi de {}kg, peso de {}.')
