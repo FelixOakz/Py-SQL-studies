7fa3e8d98713901825cc2d05c311bba3efb0efe9
felixoakz <feliz@oakz.org>
2022-09-25 11:46:34 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#085 - Listas com pares e ímpares.py
+++ PyPracticing/Guanabara/#085 - Listas com pares e ímpares.py
@@ -3,3 +3,10 @@ pares = []
 impares = []
 for i in range(1, 8):
 	numeros.append(int(input(f'Digite o {i} valor: ')))
+for i in numeros:
+	if i % 2 == 0:
+		pares.append()
+	else:
+		impares.append()
+print(pares)
+print(impares)
