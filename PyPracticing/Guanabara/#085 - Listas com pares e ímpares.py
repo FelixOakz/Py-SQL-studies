a3edfb398b720ed88b97b68d2054200e5099ef0e
felixoakz <feliz@oakz.org>
2022-09-25 12:04:17 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#085 - Listas com pares e ímpares.py
+++ PyPracticing/Guanabara/#085 - Listas com pares e ímpares.py
@@ -2,11 +2,9 @@ numeros = [[], []]
 v = 0
 for i in range(1, 8):
 	v = int(input(f'Digite o {i} valor: '))
-for i in numeros:
 	if i % 2 == 0:
-		numeros[0].append(i)
+		numeros[0].append(v)
 	else:
-		numeros[1].append(i)
-
-print(f'Numeros pares foram {pares}')
-print(f'Numeros impares foram {impares}')
+		numeros[1].append(v)
+numeros.sort()
+print(f'Todos os valores: {numeros}')
