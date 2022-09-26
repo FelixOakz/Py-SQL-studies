db53950836c38eb75665d6afe9270dfce756da83
felixoakz <feliz@oakz.org>
2022-09-26 12:30:03 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#088 - Palpites para a Mega Sena.py
+++ PyPracticing/Guanabara/#088 - Palpites para a Mega Sena.py
@@ -3,13 +3,13 @@ from time import time
 jogos = []
 lista = []
 print('-'*30, '        JOGOS NA MEGA','-'*30, sep='\n')
-jogo = int(input('Quantod jogos voce quer?: '))
+jogo = int(input('Quantos jogos voce quer?: '))
 
-print(f'Sorteando {jogo} jogos.')
+print(f'\nSorteando {jogo} jogos:')
 for i in range(jogo):
 	for i in range(6):
 		lista.append(randint(0, 60))
 	jogos.append(lista[:])
 	lista.clear()
 for i in range(jogo):
-	print('Jogo:', *jogos[i])
+	print(f'Jogo {i+1}:', *jogos[i])
