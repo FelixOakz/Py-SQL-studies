57985841712af4ae40b888bb320517728b8e39c6
felixoakz <feliz@oakz.org>
2022-09-26 09:21:40 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#087 - Mais sobre Matriz em Python.py
+++ PyPracticing/Guanabara/#087 - Mais sobre Matriz em Python.py
@@ -1,4 +1,5 @@
 m = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+par = 0
 for i in range(0, 3):
 	for j in range(0, 3):
 		m[i][j] = int(input(f'Digite um valor para [{i}, {j}]:'))
@@ -6,5 +7,6 @@ for i in range(0, 3):
 	for j in range(0, 3):
 		print(f'[{m[i][j]:^5}]', end='')
 	print()
-
-	
+print(f'A soma dos valores pares eh {par}')
+print(f'A soma dos valores da segunda coluna eh de {}.')
+print(f'O maior valor da segunda linha eh {}')
