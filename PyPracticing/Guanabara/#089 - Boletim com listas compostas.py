60c3ab80e46727079eadc137df12a8ada5405c0a
felixoakz <feliz@oakz.org>
2022-09-27 11:33:48 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
+++ PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
@@ -1,27 +1,23 @@
 geral = []
 temp = []
 r = ''
-
 while r in 'Ss':
 	temp.append(str(input('Nome: ')))
-	temp.append(int(input('Nota 1: ')))
-	temp.append(int(input('Nota 2: ')))
+	temp.append(float(input('Nota 1: ')))
+	temp.append(float(input('Nota 2: ')))
 	temp.append((temp[1] + temp[2])/2)
 	geral.append(temp[:])
 	temp.clear()
 	r = str(input('Deseja adicionar mais um? [s/n]: '))
-
-print('='*30, 'No.''NOME''MEDIA', '='*30, sep='\n')
-
+print('='*30, 'No.   NOME   MEDIA', '='*30, sep='\n')
 for i in range(len(geral)):
-	print(f'{i} {geral[i][0]}{geral[i][3]}')
+	print(f'{i}{geral[i][0]}{geral[i][3]}')
 print('-'*30)
-
 while True:
-	if r == 999:
-		print('Programa Boletim Encerrado')
-		break
-	else:
-		r = int(input('Mostrar notas de qual aluno?: '))
+	r = int(input('Mostrar notas de qual aluno? [999 p/ sair]: '))
+	if r != 999:
 		print(F'>>> As notas de {geral[r][0]} sao {geral[r][1]} e {geral[r][2]}')
 		print('-' * 30)
+	if r == 999:
+		print('>>> Programa encerrado.')
+		break
