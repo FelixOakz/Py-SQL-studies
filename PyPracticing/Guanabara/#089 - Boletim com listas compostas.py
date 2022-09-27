2bd9236d5c0004122b17acfce7d7ce5d6bf055af
felixoakz <feliz@oakz.org>
2022-09-27 11:20:37 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
+++ PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
@@ -1,6 +1,7 @@
 geral = []
 temp = []
 r = ''
+
 while r in 'Ss':
 	temp.append(str(input('Nome: ')))
 	temp.append(int(input('Nota 1: ')))
@@ -9,11 +10,18 @@ while r in 'Ss':
 	geral.append(temp[:])
 	temp.clear()
 	r = str(input('Deseja adicionar mais um? [s/n]: '))
-print('='*30)
-print('No.', 'NOME', 'MEDIA', sep='     ')
-print('-'*30)
-for i in (0, len(geral)):
-	print(f'{i}     {geral[i][0]}     ')
+
+print('='*30, 'No.''NOME''MEDIA', '='*30, sep='\n')
+
+for i in range(len(geral)):
+	print(f'{i} {geral[i][0]}{geral[i][3]}')
 print('-'*30)
 
-#print(boletim formatado com nome e media)
+while True:
+	if r == 999:
+		print('Programa Boletim Encerrado')
+		break
+	else:
+		r = int(input('Mostrar notas de qual aluno?: '))
+		print(F'>>> As notas de {geral[r][0]} sao {geral[r][1]} e {geral[r][2]}')
+		print('-' * 30)
