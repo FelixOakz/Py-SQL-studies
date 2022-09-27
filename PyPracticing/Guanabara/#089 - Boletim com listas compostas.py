7713562bf7f89d6278fb6f18040d22f793728476
felixoakz <feliz@oakz.org>
2022-09-27 10:50:19 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
+++ PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
@@ -5,9 +5,15 @@ while r in 'Ss':
 	temp.append(str(input('Nome: ')))
 	temp.append(int(input('Nota 1: ')))
 	temp.append(int(input('Nota 2: ')))
+	temp.append((temp[1] + temp[2])/2)
 	geral.append(temp[:])
 	temp.clear()
 	r = str(input('Deseja adicionar mais um? [s/n]: '))
-
+print('='*30)
+print('No.', 'NOME', 'MEDIA', sep='     ')
+print('-'*30)
+for i in (0, len(geral)):
+	print(f'{i}     {geral[i][0]}     ')
+print('-'*30)
 
 #print(boletim formatado com nome e media)
