8521479691c64dd9f2c7caeddc717488af670bb9
felixoakz <feliz@oakz.org>
2022-09-27 11:41:10 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
+++ PyPracticing/Guanabara/#089 - Boletim com listas compostas.py
@@ -9,14 +9,14 @@ while r in 'Ss':
 	geral.append(temp[:])
 	temp.clear()
 	r = str(input('Deseja adicionar mais um? [s/n]: '))
-print('='*30, 'No.   NOME   MEDIA', '='*30, sep='\n')
+print('='*30, 'No.     NOME    MEDIA', '='*30, sep='\n')
 for i in range(len(geral)):
-	print(f'{i}{geral[i][0]}{geral[i][3]}')
+	print(f'{i:<5}{geral[i][0]:^10}{geral[i][3]:>5}')
 print('-'*30)
 while True:
 	r = int(input('Mostrar notas de qual aluno? [999 p/ sair]: '))
 	if r != 999:
-		print(F'>>> As notas de {geral[r][0]} sao {geral[r][1]} e {geral[r][2]}')
+		print(F'>>> As notas de {geral[r][0]} são {geral[r][1]} e {geral[r][2]}')
 		print('-' * 30)
 	if r == 999:
 		print('>>> Programa encerrado.')
