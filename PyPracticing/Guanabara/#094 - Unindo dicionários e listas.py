3a9fd2a5a55a21d7f7773d5d5b567be7da6758fb
felixoakz <feliz@oakz.org>
2022-10-02 10:13:29 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ PyPracticing/Guanabara/#094 - Unindo dicionários e listas.py
@@ -0,0 +1,14 @@
+"""Crie um programa que leia nome, Sexo e idade de várias
+pessoas, guardando os dados de cada
+pessoa em um
+dicionário a todos os
+dicionários em uma
+lista. No final, mostra:
+A) Quantas pessoas foram cadastradas
+B) A média de idade do
+grupo. C) Uma lista com todas
+as mulheres.
+D) Uma lista com todas
+as pessoas com idade acima da média.
+"""
+
