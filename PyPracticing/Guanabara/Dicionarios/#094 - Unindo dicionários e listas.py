85d234de9bc38b57b1befa051b3f056eedd3a73c
felixoakz <feliz@oakz.org>
2022-10-08 08:57:35 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Dicionarios/#094 - Unindo dicionários e listas.py
+++ PyPracticing/Guanabara/Dicionarios/#094 - Unindo dicionários e listas.py
@@ -1,32 +1,29 @@
-"""Crie um programa que leia nome, Sexo e idade de váriastemp
-pessoas, guardando os dados de cada
-pessoa em um
-dicionário a todos os
-dicionários em uma
-lista. No final, mostra:
-A) Quantas pessoas foram cadastradas
+"""
 B) A média de idade do
 grupo.
-C) Uma lista com todas
-as mulheres.
 D) Uma lista com todas
 as pessoas com idade acima da média.
 """
 
 dict = {}
 lista = []
-m = []
+age = 0
 while True:
 	dict['nome'] = str(input('Nome: '))
 	dict['sexo'] = str(input('Sexo: ')).upper()[0]
-	if dict['sexo'] == 'F':
-		m.append()
 	dict['idade'] = int(input('Idade: '))
+	age += dict['idade']
 	lista.append(dict.copy())
-
 	r = str(input('Deseja continuar?[s/n]: ')).upper()[0]
 	if r != 'S':
 		break
+print()
 print(lista)
+print()
 print(f'Foram cadastradas {len(lista)} pessoas.')
-print(f'Lista com mulheres: {m}')
+print(f'A media de idade {age/len(lista):.1f}')
+print(f'Lista com mulheres: ',end='')
+for i in lista:
+	if i['sexo'] == 'F':
+		print(f'{i["nome"]}', end=' ')
+print()
