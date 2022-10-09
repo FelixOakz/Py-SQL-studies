9ce58104c31cdf27f765309c44dbe596f4c61b53
felixoakz <feliz@oakz.org>
2022-10-09 12:04:53 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/crack.py
+++ PyPracticing/crack.py
@@ -1,7 +1,8 @@
-from string import digits
-# giving access to all decimal digits
+
+from string import ascii_letters, digits, punctuation
+# giving access to all ascii characters, decimal digits and punctuation marks
 from itertools import product
 # habillity to cross product of all numbers
 
-for passcode in product(digits, repeat=4):
+for passcode in product(ascii_letters + digits + punctuation, repeat=8):
 	print(*passcode)
