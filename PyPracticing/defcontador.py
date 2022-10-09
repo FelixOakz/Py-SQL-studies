6049983cde4019040991107688d9d34f5ab95272
felixoakz <feliz@oakz.org>
2022-10-09 19:20:30 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/defcontador.py
+++ PyPracticing/defcontador.py
@@ -1,14 +1,15 @@
 def counter(start, end, step):
 	"""
-	a counting function and prints on screen:
-	Start parameter: beginning of counter
-	end parameter: end of couting
-	step parameter: step of counting
-	return: no return
+	a counting function and prints on screen
+	:param start: beginning of counter
+	:param end: end of couting
+	:param step: step of counting
+	:return: no return
 	"""
 	c = start
 	while c <= end:
 		print(f'{end}', end='')
 		c += step
 	print('Finished.')
+
 
