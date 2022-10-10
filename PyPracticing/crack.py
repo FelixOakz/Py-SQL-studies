aa74a03fa93f525d546e055030274b86258f7efa
felixoakz <feliz@oakz.org>
2022-10-10 07:15:29 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/crack.py
+++ PyPracticing/crack.py
@@ -11,14 +11,17 @@ for passcode in product(digits, repeat=4):
 
 	print(*passcode)
 '''
+
 import itertools
 '''
 map() converts our input into integers to match the type of the contents of 'a' 
 and tuple() makes it a tuple to match the type of combinations.
 '''
-password = tuple(map(int, input('Pass: ')))
+
+password = tuple(map(int, input('Enter 4 digit password: ')))
+
 digits = list(range(0, 10))
 
 for passcode in itertools.product(digits, repeat=4):
-    if passcode == password:
-        print("The password is: " + ''.join(str(i) for i in password))
+	if passcode == password:
+		print("The password is: " + ''.join(str(i) for i in password))
