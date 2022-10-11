79fa914395ad2e0db49ce839d10b240b06bf3e3b
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-11 18:21:25 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Dicionarios/#095 - Aprimorando os Dicionários.py
+++ PyPracticing/Guanabara/Dicionarios/#095 - Aprimorando os Dicionários.py
@@ -1,21 +1,33 @@
-"""Aprimora o DESAFIO 093 para que ele funcione com vários jogadores, incluindo um sistema da Visualização da
-detalhes do
-aproveitamento de
-cada jogador."""
-
+# bugado, mostra a mesma quantidade de gols para todos jogadores.
+team = []
 dict = {}
 list = []
-dict['nome'] = str(input('Nome do jogador: '))
-partidas = int(input('Quantas partidas jogou?: '))
-for i in range(partidas):
-	gols = int(input(f'Quantos gols na partida {i+1}?'))
-	list.append(gols)
-dict['gols'] = list
-dict['total'] = sum(list)
+
+while True:
+	dict.clear()
+	dict['nome'] = str(input('Nome do jogador: '))
+	partidas = int(input('Quantas partidas jogou?: '))
+	list.clear()
+	for i in range(partidas):
+		gols = int(input(f'Quantos gols na partida {i+1}?'))
+		list.append(gols)
+	dict['gols'] = list
+	dict['total'] = sum(list)
+	team.append(dict.copy())
+	r = str(input('Deseja continuar?')).upper()[0]
+	if r in 'N':
+		break
+print('-'*30)
+for i, j in enumerate(team):
+	print(f'{i:>3}', end='')
+	for d in j.values():
+		print(f'{str(d):<15}', end='')
+	print()
 print(dict)
 print('-'*30)
 for i, j in dict.items():
 	print(f'O campo {i} tem o valor {j}')
+
 print('-' * 30)
 print(f'O jogador {dict["nome"]} jogou {len(dict["gols"])} partidas.')
 for i, j in enumerate(dict['gols']):
