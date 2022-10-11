2c4df7b5a496a7c9f32f3018a96845ebbc3c2763
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-11 19:13:50 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Funcoes/#101 - Funções para votação.py
+++ PyPracticing/Guanabara/Funcoes/#101 - Funções para votação.py
@@ -1,10 +1,12 @@
 def voto(nasc):
 	from datetime import date
 	age = date.today().year - nasc
-	if age < 18:
-		print(f'Com {age} anos: NAO VOTA.')
+	if age < 16:
+		return f'Com {age} anos: NAO VOTA.'
+	elif 16 <= age < 18 or age > 65:
+		return f'Com {age} anos: VOTO OPCIONAL.'
 	else:
-		print(f'Com {age} anos: VOTO OBRIGATORIO.')
+		return f'Com {age} anos: VOTO OBRIGATORIO.'
 
 
-voto(int(input('what year were you born?: ')))
+voto(int(input('Que ano vc nasceu?: ')))
