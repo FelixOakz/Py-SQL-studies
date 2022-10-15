2cf647b8edd2e99ec50c6b704682ad660eb4e126
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-14 21:05:58 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/WifiQrCode.py
+++ PyPracticing/WifiQrCode.py
@@ -1,8 +1,8 @@
 import wifi_qrcode_generator
 
-ssid = input('Home wifi')
+ssid = input('Nome da rede: ')
+password = input('Senha da rede: ')
 type = 'WPA'
-password = input('very complicated password')
 
 img = wifi_qrcode_generator.wifi_qrcode(ssid, False, type , password)
 img.save(ssid+'.png')
