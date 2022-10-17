79794aa221293bc8e23fcc410d6745c19fd2e313
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-17 20:23:04 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Funcoes/#103 - Ficha do Jogador.py
+++ PyPracticing/Guanabara/Funcoes/#103 - Ficha do Jogador.py
@@ -1,6 +1,13 @@
-def ficha(nome='nao informado', gols=0):
-	nome = str(input('Nome do jogador?: '))
-	gols = int(input('Numero de gols?: '))
-	print(f'O jogador {nome} fez {gols}gol(s) no campeonato')
+def ficha(nome, gols=0):
+	print(f'O jogador {nome} fez {gols} gol(s) no campeonato')
 
-ficha()
+
+nome = str(input('Nome do jogador?: '))
+gols = input('Numero de gols?: ')
+if nome == '':
+	ficha('Desconhecido', gols)
+else:
+	if gols.isnumeric():
+		ficha(nome, gols)
+	else:
+		ficha(nome)
