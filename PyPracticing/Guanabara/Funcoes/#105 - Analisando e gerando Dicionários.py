29646fe2e5d1d3114cb66192ac5e7f105f925baf
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-17 20:49:33 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Funcoes/#105 - Analisando e gerando Dicionários.py
+++ PyPracticing/Guanabara/Funcoes/#105 - Analisando e gerando Dicionários.py
@@ -1,4 +1,4 @@
-def notas(*not, sit=False):
+def notas(*n, sit=False):
 	"""
 	-> funcao para analisar notas e situacoes de varios alunos
 	:param not: uma ou mais notas dos alunos
@@ -6,19 +6,20 @@ def notas(*not, sit=False):
 	:return: dicionario com varias infos
 	"""
 	r = {}
-	r['Total'] = len(not)
-	r['Maior'] = max(not)
-	r['Menor'] = min(not)
-	r['Media'] = sum(not)/len(not)
+	r['Total'] = len(n)
+	r['Maior'] = max(n)
+	r['Menor'] = min(n)
+	r['Media'] = sum(n)/len(n)
 	if sit:
-		if r['média'] >= 7:
+		if r['Media'] >= 7:
 			r['situação'] = 'BOA'
-	elif
-		r['média'] >= 5:
-	r['situação'] = 'RAZOÁVEL'
-	else:
-	r['situação'] = 'RUIM'
+		elif r['Media'] >= 5:
+				r['situacao'] = 'RAZOÁVEL'
+		else:
+			r['situação'] = 'RUIM'
 	return r
 
 
-resp = notas(5.5, 2.5, 9, 8.5)
+resp = notas(5.5, 2.5, 9, 8.5, sit=True)
+print(resp)
+help(notas)
