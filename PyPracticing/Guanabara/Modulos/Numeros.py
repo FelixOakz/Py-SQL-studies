b1e2beb72aefe7b0d4982112824fb74ec2fc8484
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-20 08:15:25 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Modulos/Numeros.py
+++
@@ -1,9 +0,0 @@
-def fatorial(n):
-	f = 1
-	for i in range(1, n+1):
-		f *= i
-	return f
-
-
-num = int(input('Type a number to find its factorial: '))
-print(f'Factorial of {num} equals {fatorial(num)}')
