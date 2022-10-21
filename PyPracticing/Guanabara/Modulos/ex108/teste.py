0aa87491b2114f93961c2b05dfc9d0bf23ab4dbd
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-21 20:55:35 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Modulos/ex108/teste.py
+++ PyPracticing/Guanabara/Modulos/ex108/teste.py
@@ -1,7 +1,7 @@
 import moeda
 
 p = float(input('Type the value: R$'))
-print(f'A metade de {p} é {moeda.metade(p)}')
-print(f'O dobro de {p} equivale a {moeda.dobro(p)}')
-print(f'Aumento de 10%, temos {moeda.aumentar(p, 10)}')
-print(f'Reduzindo 13%, temos {moeda.diminuir(p, 13)}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
+print(f'O dobro de {moeda.moeda(p)} equivale a {moeda.moeda(moeda.dobro(p))}')
+print(f'Aumento de 10%, temos {moeda.moeda(moeda.aumentar(p, 10))}')
+print(f'Reduzindo 13%, temos {moeda.moeda(moeda.diminuir(p, 13))}')
