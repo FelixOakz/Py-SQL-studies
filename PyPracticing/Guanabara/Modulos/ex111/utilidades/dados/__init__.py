29f448d5958b78466f9875030701144a05a11367
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-23 17:09:07 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Modulos/ex111/utilidades/dados/__init__.py
+++ PyPracticing/Guanabara/Modulos/ex111/utilidades/dados/__init__.py
@@ -7,3 +7,18 @@ def readvalue(msg):
 		else:
 			valido = True
 			return float(ent)
+
+
+def leiaInt(msg):
+	ok = False
+	valor = 0
+	while True:
+		n = str(input(msg))
+		if n.isnumeric():
+			valor = int(n)
+			ok = True
+		else:
+			print('\033[0;31m Erro, digite um valor valido.\033[m')
+		if ok:
+			break
+	return valor
