13bf8d624fef776c54a03596ec7812493b4e8ded
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-23 09:31:03 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Modulos/ex109/moeda.py
+++ PyPracticing/Guanabara/Modulos/ex109/moeda.py
@@ -1,19 +1,19 @@
-def aumentar(p, val):
-	val = val / 100
-	return p + (p * val)
+def aumentar(p, val, formato=False):
+	res = p + (p * val / 100)
+	return res if formato is False else moeda(res)
+
+def diminuir(p, val, formato=False):
+	res = p - (p * val / 100)
+	return res if formato is False else moeda(res)
 
 
-def diminuir(p, val):
-	val = val / 100
-	return p - (p * val)
+def dobro(p, formato=False):
+	res = p * 2
+	return res if not formato else moeda(res)
 
-
-def dobro(p):
-	return p * 2
-
-
-def metade(p):
-	return p / 2
+def metade(p, formato=False):
+	res = p / 2
+	return res if not formato else moeda(res)
 
 
 def moeda(val):
