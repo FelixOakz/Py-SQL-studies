4f2a8acc8349ae15046f757f2b94a56cd72f3dc7
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-23 17:07:05 -0300

    Merge remote-tracking branch 'origin/master'

--- PyPracticing/Guanabara/Modulos/ex111/utilidades/dados/__init__.py
+++ PyPracticing/Guanabara/Modulos/ex111/utilidades/dados/__init__.py
@@ -2,7 +2,7 @@ def readvalue(msg):
 	valido = False
 	while not valido:
 		ent = str(input(msg)).replace(',', '.')
-		if ent.isalpha():
+		if ent.isalpha() or ent.strip() == '':
 			print(f'\033[0;31mERROR {ent}: Invalid command.\033[m')
 		else:
 			valido = True
