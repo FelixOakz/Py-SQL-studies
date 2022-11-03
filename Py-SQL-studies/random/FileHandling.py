d8f7f94175066cb384c333bda43ba0e2240c8d0c
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-03 20:07:38 -0300

    now

--- Py-SQL-studies/random/FileHandling.py
+++ Py-SQL-studies/random/FileHandling.py
@@ -1,6 +1,6 @@
 import os
 
-filepath = os.path.abspath('Documents/emailvagas.txt')
+filepath = os.path.abspath('/home/felixoakz/code/Py-SQL-studies/random/pythonxDatabase.txt')
 
 file = open(filepath, 'r')
 print(file.read())
