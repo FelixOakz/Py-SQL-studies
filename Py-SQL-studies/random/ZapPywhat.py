a02f9a7fb8881e64e98e8b0ad6c6104afde6528e
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-04 07:38:13 -0300

    now

--- Py-SQL-studies/random/ZapPywhat.py
+++ Py-SQL-studies/random/ZapPywhat.py
@@ -2,7 +2,10 @@ import pywhatkit as zap
 
 ent = str(input('Numero: '))
 num = '+5584'+ent
+
 msg = str(input('Msg a ser enviada: '))
+
 hr = int(input('Hora: '))
 min = int(input('Min: '))
+
 zap.sendwhatmsg(num, msg, hr, min)
