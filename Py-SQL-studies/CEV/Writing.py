ded68f977fe9d64e7f7b5cadae771531550b342a
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-06 19:49:39 -0300

    now

--- Py-SQL-studies/CEV/Writing.py
+++ Py-SQL-studies/CEV/Writing.py
@@ -1,15 +1,3 @@
-import os
-
-filepath = os.path.abspath('/Py-SQL-studies/random/TextDatabase.txt')
-
-for i in range(1, 1001):
-	file = open(filepath, 'a')
-	file.write(f'teste numero {i}\n')
-	file.close()
-
-print('process finished')
-
-
 
 """
 menu principal, ver pessoas cadastradas, cadastrar nova pessoa, sair
@@ -20,9 +8,32 @@ menu
 adiconar gente
 """
 
+def writing(nome, idade):
+	import os
+	filepath = os.path.abspath('/Py-SQL-studies/random/TextDatabase.txt')
+	file = open(filepath, 'a')
+	file.write(f'{nome}, {idade}')
+	file.close()
+
+def reading():
+	import os
+	filepath = os.path.abspath('/Py-SQL-studies/random/TextDatabase.txt')
+	file = open(filepath, 'r')
+	file.read()
+
+
 def printar(text):
 	tam = len(text) + 4
 	print(f'{"-"*tam}\n  {text}\n{"-"*tam}')
 
-def
+def separador(simb):
+	print(f'{simb}'*30)
 
+
+
+
+
+printar('MENU PRINCIPAL')
+print('1 - Ver pessoas cadastradas')
+print('2 - Cadastrar nova pessoa')
+print('3 - Sair do sistema')
