b8b50a02e4851573f76c9b6659be6d9db0e5506b
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-06 20:09:50 -0300

    ex 15

--- Py-SQL-studies/CEV/Writing.py
+++ Py-SQL-studies/CEV/Writing.py
@@ -11,22 +11,23 @@ adiconar gente
 
 def writing(nome, idade):
 	import os
-	filepath = os.path.abspath('/Py-SQL-studies/random/TextDatabase.txt')
+	filepath = os.path.abspath('cevdatabase.txt')
 	file = open(filepath, 'a')
 	file.write(f'{nome}, {idade}')
-	file.close()
+	file.close() 
 
 
 def reading():
 	import os
-	filepath = os.path.abspath('/Py-SQL-studies/random/TextDatabase.txt')
+	filepath = os.path.abspath('cevdatabase.txt')
 	file = open(filepath, 'r')
 	file.read()
 
 
 def printar(text):
-	tam = len(text) + 4
-	print(f'{"-"*tam}\n  {text}\n{"-"*tam}')
+	print('-'*30)
+	print(text)
+	print('-'*30)
 
 
 def separador(simb):
@@ -37,3 +38,7 @@ printar('MENU PRINCIPAL')
 print('1 - Ver pessoas cadastradas')
 print('2 - Cadastrar nova pessoa')
 print('3 - Sair do sistema')
+option = int(input('Type your option: '))
+if option == 1:
+	reading()
+
