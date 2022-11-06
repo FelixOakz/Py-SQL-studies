2bf953aba14c3eec7d55e4bac678b293f2ff21f1
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-06 20:12:57 -0300

    oi

--- Py-SQL-studies/CEV/Writing.py
+++ Py-SQL-studies/CEV/Writing.py
@@ -21,7 +21,7 @@ def reading():
 	import os
 	filepath = os.path.abspath('cevdatabase.txt')
 	file = open(filepath, 'r')
-	file.read()
+	print(file.read())
 
 
 def printar(text):
@@ -38,7 +38,6 @@ printar('MENU PRINCIPAL')
 print('1 - Ver pessoas cadastradas')
 print('2 - Cadastrar nova pessoa')
 print('3 - Sair do sistema')
-option = int(input('Type your option: '))
+option = int(input('Escolha opcao: '))
 if option == 1:
 	reading()
-
