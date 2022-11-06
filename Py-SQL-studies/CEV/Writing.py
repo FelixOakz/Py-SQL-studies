13ab4e972e1c198874b59f59a2cb88b9995787b4
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-06 20:24:02 -0300

    spaces

--- Py-SQL-studies/CEV/Writing.py
+++ Py-SQL-studies/CEV/Writing.py
@@ -10,28 +10,28 @@ adiconar gente
 
 
 def writing(nome, idade):
-	import os
-	filepath = os.path.abspath('cevdatabase.txt')
-	file = open(filepath, 'a')
-	file.write(f'{nome}, {idade}')
-	file.close() 
+    import os
+    filepath = os.path.abspath('cevdatabase.txt')
+    file = open(filepath, 'a')
+    file.write(f'{nome}, {idade}')
+    file.close() 
 
 
 def reading():
-	import os
-	filepath = os.path.abspath('cevdatabase.txt')
-	file = open(filepath, 'r')
-	print(file.read())
+    import os
+    filepath = os.path.abspath('cevdatabase.txt')
+    file = open(filepath, 'r')
+    print(file.read())
 
 
 def printar(text):
-	print('-'*30)
-	print(text)
-	print('-'*30)
+    print('-'*30)
+    print(text)
+    print('-'*30)
 
 
 def separador(simb):
-	print(f'{simb}'*30)
+    print(f'{simb}'*30)
 
 
 printar('MENU PRINCIPAL')
@@ -39,15 +39,18 @@ print('1 - Ver pessoas cadastradas')
 print('2 - Cadastrar nova pessoa')
 print('3 - Sair do sistema')
 while True:
-	option = int(input('Escolha opcao: '))
-	if option == 1:
-		reading()
-	elif option == 2:
-	    nome = str(input('Digite o nome: ')).strip().capitalize()
-	    idade = int(input('Digite a idade: '))
-	    writing(nome, idade)
-	elif option == 3:
-	    printar('Saindo do sistema....')
-	    break
-	else:
-		print('Escolha uma opcao adequada!')
+    option = int(input('Escolha opcao: '))
+    if option == 1:
+        reading()
+  
+    elif option == 2:
+        nome = str(input('Digite o nome: ')).strip().capitalize()
+        idade = int(input('Digite a idade: '))
+        writing(f'\n{nome}, {idade}')
+    
+    elif option == 3:
+        printar('Saindo do sistema....')
+        break
+    else:
+        print('Escolha uma opcao adequada!')
+        
