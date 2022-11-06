b406e82e842c6b3d4765869bec7e56fa7014418c
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-06 20:19:23 -0300

    again

--- Py-SQL-studies/CEV/Writing.py
+++ Py-SQL-studies/CEV/Writing.py
@@ -38,6 +38,16 @@ printar('MENU PRINCIPAL')
 print('1 - Ver pessoas cadastradas')
 print('2 - Cadastrar nova pessoa')
 print('3 - Sair do sistema')
-option = int(input('Escolha opcao: '))
-if option == 1:
-	reading()
+while True:
+	option = int(input('Escolha opcao: '))
+	if option == 1:
+		reading()
+	elif option == 2:
+	    nome = str(input('Digite o nome: ')).strip().capitalize()
+	    idade = int(input('Digite a idade: '))
+	    writing(nome, idade)
+	elif option == 3:
+	    printar('Saindo do sistema....')
+	    break
+	else:
+		print('Escolha uma opcao adequada!')
