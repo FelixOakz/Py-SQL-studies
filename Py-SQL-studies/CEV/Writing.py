7423db9a6dcc373f78e2fca1195398f2fa478034
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-06 20:30:52 -0300

    almost

--- Py-SQL-studies/CEV/Writing.py
+++ Py-SQL-studies/CEV/Writing.py
@@ -13,7 +13,7 @@ def writing(nome, idade):
     import os
     filepath = os.path.abspath('cevdatabase.txt')
     file = open(filepath, 'a')
-    file.write(f'{nome}, {idade}')
+    file.write(f'\n{nome}, {idade}')
     file.close() 
 
 
@@ -34,7 +34,7 @@ def separador(simb):
     print(f'{simb}'*30)
 
 
-printar('MENU PRINCIPAL')
+printar('     MENU PRINCIPAL')
 print('1 - Ver pessoas cadastradas')
 print('2 - Cadastrar nova pessoa')
 print('3 - Sair do sistema')
@@ -46,11 +46,10 @@ while True:
     elif option == 2:
         nome = str(input('Digite o nome: ')).strip().capitalize()
         idade = int(input('Digite a idade: '))
-        writing(f'\n{nome}, {idade}')
+        writing(nome, idade)
     
     elif option == 3:
         printar('Saindo do sistema....')
         break
     else:
         print('Escolha uma opcao adequada!')
-        
