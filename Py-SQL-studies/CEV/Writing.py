28872f0b825ae510e2c95ecfe9f8ea21d6da17ab
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-09 08:42:13 -0300

    now

--- Py-SQL-studies/CEV/Writing.py
+++ Py-SQL-studies/CEV/Writing.py
@@ -1,21 +1,29 @@
 def writing(nome, idade):
-    import os
-    filepath = os.path.abspath('CEV/cevdatabase.txt')
-    file = open(filepath, 'a')
-    file.write(f'\n{nome}, {idade}')
-    file.close() 
+    try:
+        import os
+        filepath = os.path.abspath('cevdatabase.csv')
+        file = open(filepath, 'a')
+        file.write(f'\n{nome}, {idade}')
+        file.close()
+    except IOError:
+        print('\033[0;31mFile not found or path is incorrect!\033[m\n')
 
 
 def reading():
-    import os
-    filepath = os.path.abspath('CEV/cevdatabase.txt')
-    file = open(filepath, 'r')
-    print(file.read())
+    try:
+        import os
+        filepath = os.path.abspath('cevdatabase.csv')
+        file = open(filepath, 'r')
+        print(file.read())
+        print('-'*30)
+    except IOError:
+        print('\033[0;31mFile not found or path is incorrect!\033[m\n')
+
 
 
 def printar(text):
     print('-'*30)
-    print(text)
+    print(text.center(30))
     print('-'*30)
 
 
@@ -27,6 +35,7 @@ if __name__ == '__main__':
     while True:
         option = input('Escolha opcao: ')
         if option == '1':
+            printar('Lista de nomes:')
             reading()
     
         elif option == '2':
@@ -35,9 +44,7 @@ if __name__ == '__main__':
             writing(nome, idade)
         
         elif option == '3':
-            printar('Saindo do sistema....')
+            printar('Saindo do sistema')
             break
         else:
             print('\033[0;31mEscolha uma opcao adequada!\033[m')
-
-        
