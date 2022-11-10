b664a289d46a1d1c6948fdb7fcef2bff2946b9ef
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-10 18:04:05 -0300

    now

--- Py-SQL-studies/CEV/sisteminha.py
+++ Py-SQL-studies/CEV/sisteminha.py
@@ -1,9 +1,9 @@
-def writing(nome, idade):
+def writing(nome='desconhecido', idade=0):
     try:
         import os
         filepath = os.path.abspath('cevdatabase.txt')
-        file = open(filepath, 'a')
-        file.write(f'\n{nome},{idade}')
+        file = open(filepath, 'at')
+        file.write(f'{nome},{idade}\n')
         file.close()
     except IOError:
         print('\033[0;31mFile not found or path is incorrect!\033[m\n')
@@ -13,14 +13,13 @@ def reading():
     try:
         import os
         filepath = os.path.abspath('cevdatabase.txt')
-        file = open(filepath, 'r')
+        file = open(filepath, 'r+')
         for linha in file:
             data = linha.split(',')
             data[1] = data[1].replace('\n', '')
             print(f'{data[0]:<34}{data[1]:>3} anos')
-        print(line())
     except IOError:
-        print('\033[0;31mFile not found or path is incorrect!\033[m\n')
+        print('\033[0;31m - Nao ha pessoas cadastradas!\033[m')
 
 
 def line(tam = 42):
@@ -37,19 +36,29 @@ def separador(simb):
     print(f'{simb}'*30)
 
 
+def menu():
+    header('Menu Principal')
+    print('1 - Ver pessoas cadastradas')
+    print('2 - Cadastrar nova pessoa')
+    print('3 - Sair do programa')
+    print(line())
+
+
 if __name__ == '__main__':
     while True:
+        menu()
         option = input('Escolha opcao: ')
-        print(line())
         if option == '1':
+            header('Pessoas cadastradas:')
             reading()
 
         elif option == '2':
-            header('cadastrar pessoa:')
+            header('Cadastrar pessoa:')
             nome = str(input('Digite o nome: ')).strip().capitalize()
             idade = int(input('Digite a idade: '))
             writing(nome, idade)
-        
+            print('\033[0;32m\n   Registro adicionado com sucesso!\033[m')
+
         elif option == '3':
             header('Saindo do sistema')
             break
