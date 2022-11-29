37b83e046606b489b6554901e1b15c22e16c5264
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-29 11:48:55 -0300

    now

--- Py-SQL-studies/CEV/draft.py
+++ Py-SQL-studies/CEV/draft.py
@@ -1,4 +1,1 @@
-arr = [4, 1, 5, 2, 3]
-arrmax = sum(arr) - max(arr)
-arrmin = sum(arr) - min(arr)
-print(arrmax, arrmin)
+print((loc + rot) % 25)
--- Py-SQL-studies/hackerrank/caesarchyper.py
+++ Py-SQL-studies/hackerrank/caesarchyper.py
@@ -1,17 +1,14 @@
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 s = 'middle-Outz'
 rot = 2
-
 for letter in s:
     if letter.isalpha():
         if letter.islower():
-            loc = alpha.find(letter)
-            loc += rot
-            print(letter, alpha[loc])
+            loc = (alpha.find(letter) + rot) % 26
+            print(f'{alpha[loc]}', end='')
         else:
             letter = letter.lower()
-            loc = alpha.find(letter)
-            loc += rot
-            print(letter, alpha[loc].upper())
+            loc = (alpha.find(letter) + rot) % 26
+            print(f'{alpha[loc].upper()}', end='')
     else:
-        print(letter)
+        print(f'{letter}', end='')
