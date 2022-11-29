a0f7d7cc2f7fc6796aa33dbfd429297776880e77
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-29 10:37:10 -0300

    advancing in a new cipher

--- Py-SQL-studies/hackerrank/caesarchyper.py
+++ Py-SQL-studies/hackerrank/caesarchyper.py
@@ -2,7 +2,16 @@ alpha = 'abcdefghijklmnopqrstuvwxyz'
 s = 'middle-Outz'
 rot = 2
 
-for i in s:
-    loc = alpha.find(i)
-    loc += rot
-    
+for letter in s:
+    if letter.isalpha():
+        if letter.islower():
+            loc = alpha.find(letter)
+            loc += rot
+            print(letter, alpha[loc])
+        else:
+            letter = letter.lower()
+            loc = alpha.find(letter)
+            loc += rot
+            print(letter, alpha[loc].upper())
+    else:
+        print(letter)
