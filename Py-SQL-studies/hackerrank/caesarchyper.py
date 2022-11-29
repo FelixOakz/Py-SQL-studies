d721430d6500f1e7fedfc6fc6381ca36a47209af
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-11-29 12:40:12 -0300

    now

--- Py-SQL-studies/hackerrank/caesarchyper.py
+++ Py-SQL-studies/hackerrank/caesarchyper.py
@@ -1,14 +1,21 @@
-alpha = 'abcdefghijklmnopqrstuvwxyz'
 s = 'middle-Outz'
-rot = 2
-for letter in s:
-    if letter.isalpha():
-        if letter.islower():
-            loc = (alpha.find(letter) + rot) % 26
-            print(f'{alpha[loc]}', end='')
+k = 2
+
+
+def caesarCipher(s, k):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    out = ''
+    for letter in s:
+        if letter.isalpha():
+            if letter.islower():
+                loc = (alpha.find(letter) + k) % 26
+                out += alpha[loc]
+            else:
+                letter = letter.lower()
+                loc = (alpha.find(letter) + k) % 26
+                out += alpha[loc].upper()
         else:
-            letter = letter.lower()
-            loc = (alpha.find(letter) + rot) % 26
-            print(f'{alpha[loc].upper()}', end='')
-    else:
-        print(f'{letter}', end='')
+            out += letter
+    print(out)
+
+caesarCipher(s, k)
