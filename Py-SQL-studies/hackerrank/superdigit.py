c2102a5910211b6481cc8fa6728e576c486507d1
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-01 19:27:30 -0300

    now

--- Py-SQL-studies/hackerrank/superdigit.py
+++ Py-SQL-studies/hackerrank/superdigit.py
@@ -1,12 +1,7 @@
-n = 9875987598759875
-k = 3
-
-n = int(n)
-while n > 9:
-	n = sum(map(int, str(n)))
-print(k)
-
-x = ((int(n) % 9) * k) % 9
-return x if x else 9
-
-# ??????
+n = str(input('enter n: '))
+k = int(input('enter k: '))
+p = n * k
+p = int(p)
+while p > 9:
+	p = sum(map(int, str(p)))
+print(p)
