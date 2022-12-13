1b20f923fb09ac01ec2bb53161a4d17801b871c0
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-13 08:59:02 -0300

    now

--- Py-SQL-studies/hackerrank/Reverse Polish Notation.py
+++ Py-SQL-studies/hackerrank/Reverse Polish Notation.py
@@ -1,5 +1,22 @@
-case = ["2","1","+","3","*"]
-# expected output: 9
 
-for i in case:
-	if i.isnumeric():
+def rpn(case):
+	stack = []
+	for i in case:
+		if i.isnumeric():
+			stack.append(int(i))
+		elif i in "+-*/":
+			n1 = stack.pop()
+			n2 = stack.pop()
+			if i == '+':
+				stack.append(n1 + n2)
+			elif i == '-':
+				stack.append(n1 - n2)
+			elif i == '*':
+				stack.append(n1 * n2)
+			elif i == '/':
+				stack.append(n1 / n2)
+	return stack.pop()
+
+
+
+print(rpn(["2","1","+","3","*"]))
