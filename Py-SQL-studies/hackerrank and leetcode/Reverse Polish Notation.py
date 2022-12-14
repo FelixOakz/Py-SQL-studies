2c8904dcc996c5a97029b87d2de3393b834f8f76
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-13 21:18:10 -0300

    now

--- Py-SQL-studies/hackerrank and leetcode/Reverse Polish Notation.py
+++ Py-SQL-studies/hackerrank and leetcode/Reverse Polish Notation.py
@@ -1,31 +1,17 @@
-def rpn(cases):
-	results = []
-	for case in cases:
+class Solution:
+	def evalRPN(self, tokens: List[str]) -> int:
 		stack = []
-		for i in case:
-			if i.isnumeric():
+		for i in tokens:
+			if i == '+':
+				stack.append(stack.pop() + stack.pop())
+			elif i == '*':
+				stack.append(stack.pop() * stack.pop())
+			elif i == '-':
+				a, b = stack.pop(), stack.pop()
+				stack.append(b - a)
+			elif i == '/':
+				a, b = stack.pop(), stack.pop()
+				stack.append(int(b / a))
+			else:
 				stack.append(int(i))
-			elif i in "+-*/":
-				n1 = stack.pop()
-				n2 = stack.pop()
-				if i == '+':
-					stack.append(n1 + n2)
-				elif i == '-':
-					stack.append(n1 - n2)
-				elif i == '*':
-					stack.append(n1 * n2)
-				elif i == '/':
-					stack.append(n1 / n2)
-		# Check if the stack has exactly one element,
-		# which should be the result of the calculation
-		if len(stack) != 1:
-			results.append("NO")
-		else:
-			results.append(stack.pop())
-	return results
-
-
-if __name__ == '__main__':
-	cases = input()
-
-	rpn(cases)
+		return stack[0]
