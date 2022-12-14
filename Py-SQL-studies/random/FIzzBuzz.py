e6c274f7c8eca53b58f31c1eb12990a4a1a45130
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-14 20:04:41 -0300

    update

--- Py-SQL-studies/random/FIzzBuzz.py
+++ Py-SQL-studies/random/FIzzBuzz.py
@@ -3,14 +3,8 @@ for i in range(1, 101):
     # condicoes diferenciadas de acordo com resultado esperado
     if i % 3 == 0 and i % 5 == 0:
         print('FizzBuzz')
-        # pular iteracoes seguintes, entregando controle de volta para loop
-        continue
     elif i % 3 == 0:
         print('Fizz')
-        continue
     elif i % 5 == 0:
         print('Buzz')
-        continue
     print(i)
-
-# that was ittt
