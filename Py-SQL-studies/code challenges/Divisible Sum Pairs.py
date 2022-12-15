295907c8c4365aafffb32bd3c0ca5398b41df1f9
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-15 09:38:05 -0300

    update

--- Py-SQL-studies/code challenges/Divisible Sum Pairs.py
+++ Py-SQL-studies/code challenges/Divisible Sum Pairs.py
@@ -3,4 +3,11 @@ k = 3
 ar = [1, 3, 2, 6, 1, 2]
 
 def divisibleSumPairs(n, k, ar):
+	pair = 0
 	for i in ar:
+		print(i)
+		for j in ar:
+			print(j)
+			if (i + j) % k == 0:
+				pair += 1
+	print(pair)
