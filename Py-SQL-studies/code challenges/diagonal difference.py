ec318983768ab6dc2133a1e827c7fd79ea5f29a9
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-16 08:42:17 -0300

    update

--- Py-SQL-studies/code challenges/diagonal difference.py
+++ Py-SQL-studies/code challenges/diagonal difference.py
@@ -1,7 +1,10 @@
-arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+#      [0] [1] [2]
+arr = [[11, 2, 4],		# [0]
+		[4, 5, 6],		# [1]
+		[10, 8, -12]]	#[2]
+
 n = 3
-primd = 0
-secd = 0
+primd = secd = 0
 for i in range(n):
 	primd += arr[i][i]
 	secd += arr[n - i - 1][i]
